5b670b4b3d3a6c89e71a8adfd61c98869ede9bbe
Deric Bunch <ddbunc2@gmail.com>
2015-08-28 14:42:28 -0500

    adding app.js

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("Pinterest", ['firebase', 'angular.filter', 'ngRoute']);
+
+// app.config(['$routeProvider',
+//   function($routeProvider) {
+//     $routeProvider.
+//       when('/song-form', {
+//         templateUrl: 'partials/song-form.html',
+//         controller: 'SongFormCtrl'
+//       }).
+//       otherwise({
+//         redirectTo: '/'
+//       });
+      
+}]);
