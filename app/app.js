f3829e89e9fdce1d055609396754112c88a7cd46
Robin Burton <1.robinburton@gmail.com>
2015-08-28 15:17:34 -0500

    addPin working

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
-var app = angular.module("Pinterest", ['firebase', 'angular.filter', 'ngRoute']);
+var app = angular.module("Pinterest", ['firebase', 'angular.filter', 'ngRoute',]);
 
-// app.config(['$routeProvider',
-//   function($routeProvider) {
-//     $routeProvider.
-//       when('/song-form', {
-//         templateUrl: 'partials/song-form.html',
-//         controller: 'SongFormCtrl'
-//       }).
-//       otherwise({
-//         redirectTo: '/'
-//       });
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/pin-form', {
+        templateUrl: 'partials/add-pin-form.html',
+        controller: 'addPinCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
       
 }]);
---
+++ app/controllers/addPins.js
@@ -0,0 +1,19 @@
+app.controller("addPinCtrl", ["$scope", "$firebaseArray",
+  function($scope, $firebaseArray) {
+
+    var ref = new Firebase("https://pinterest-project.firebaseio.com/pins");
+
+    $scope.pins = $firebaseArray(ref);
+
+    $scope.addPin = function() {
+    $scope.pins.$add({
+      "img": $scope.newPin.img, 
+      "tag": $scope.newPin.tag,
+      "title": $scope.newPin.title,
+      "url": $scope.newPin.url,
+      "uid": ""
+    });
+    $scope.newPin = {"":""};
+   };
+
+  }]);
