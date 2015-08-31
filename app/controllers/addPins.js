27fb3bdf828f1b20cb1ed4f30f106ebc9129f16c
Deric Bunch <ddbunc2@gmail.com>
2015-08-31 11:02:42 -0500

    working on OAuth

--- app/controllers/addPins.js
+++ app/controllers/addPins.js
@@ -1,8 +1,7 @@
-app.controller("addPinCtrl", ["$scope", "$firebaseArray", "$firebaseAuth",
-  function($scope, $firebaseArray, $firebaseAuth) {
+app.controller("addPinCtrl", ["$scope", "$firebaseArray",
+  function($scope, $firebaseArray) {
 
     var ref = new Firebase("https://pinterest-project.firebaseio.com/pins");
-    var auth = $firebaseAuth(ref);
 
     $scope.pins = $firebaseArray(ref);
 
@@ -16,15 +15,4 @@ app.controller("addPinCtrl", ["$scope", "$firebaseArray", "$firebaseAuth",
     });
     $scope.newPin = {"":""};
    };
-
-
- 
-ref.authWithOAuthPopup("github", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
-
-  }]);
+}]);
--- app/controllers/global.js
+++ app/controllers/global.js
@@ -1,8 +1,18 @@
-app.controller("GlobalCtrl", ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
+app.controller("GlobalCtrl", ["$scope", "$firebaseArray", "$firebaseAuth", function($scope, $firebaseArray, $firebaseAuth) {
+
+  var ref = new Firebase("https://pinterest-project.firebaseio.com/pins");
+  var auth = $firebaseAuth(ref);
+
+  ref.authWithOAuthPopup("github", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
 
   $scope.searchCategories = "";
   
-  var ref = new Firebase("https://pinterest-project.firebaseio.com/pins");
   $scope.pins = new $firebaseArray(ref);
 
 }]);
