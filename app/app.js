afefb3e984e21fa74bcc724caa90fa3a67039595
Caitlin Stein <caitlin.teter@gmail.com>
2015-08-31 10:25:54 -0500

    basic authentication

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 var app = angular.module("Pinterest", ['firebase', 'angular.filter', 'ngRoute',]);
 
+
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/pin-form', {
+      when('/', {
+        templateUrl: 'partials/global.html',
+        controller: 'GlobalCtrl'
+      }).when ('/pin-form', {
         templateUrl: 'partials/add-pin-form.html',
         controller: 'addPinCtrl'
-      }).
-      otherwise({
+      }). 
+      otherwise ({
         redirectTo: '/'
       });
       
