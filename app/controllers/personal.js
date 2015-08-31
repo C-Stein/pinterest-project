24cfacd649e4dfe897abcd5aadedf815b2dfe636
Deric Bunch <ddbunc2@gmail.com>
2015-08-31 15:15:06 -0500

    working personal view

--- app/controllers/personal.js
+++ app/controllers/personal.js
@@ -1,9 +1,10 @@
 app.controller("PersonalCtrl", ["$scope", "$firebaseArray", "uid", function($scope, $firebaseArray, uid) {
   
-  var ref = new Firebase("https://pinterest-project.firebaseio.com/pins");
+  var ref = new Firebase("https://pinterest-project.firebaseio.com");
   var currentUser = uid.getUid();
   
-  ref.orderByChild("uid").equalTo(currentUser);
-  $scope.pins = new $firebaseArray(ref);
-
+  ref.child('pins').orderByChild('uid').equalTo(currentUser).on('value', function(snapshot){
+    $scope.pins = snapshot.val();
+    console.log($scope.pins);
+  });
 }]);
