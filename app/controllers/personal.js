dad27d525506f284316891c22b8e6b05bd0225dc
Robin Burton <1.robinburton@gmail.com>
2015-08-31 14:42:56 -0500

    personal view

--- app/controllers/personal.js
+++ app/controllers/personal.js
@@ -1,6 +1,9 @@
-app.controller("PersonalCtrl", ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
+app.controller("PersonalCtrl", ["$scope", "$firebaseArray", "uid", function($scope, $firebaseArray, uid) {
   
   var ref = new Firebase("https://pinterest-project.firebaseio.com/pins");
+  var currentUser = uid.getUid();
+  
+  ref.orderByChild("uid").equalTo(currentUser);
   $scope.pins = new $firebaseArray(ref);
 
 }]);
