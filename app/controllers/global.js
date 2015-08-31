75cdd6d497f08835875feacc51a8b91dde216ab6
Deric Bunch <ddbunc2@gmail.com>
2015-08-31 10:07:39 -0500

    added search functionality

--- app/controllers/global.js
+++ app/controllers/global.js
@@ -1,5 +1,7 @@
 app.controller("GlobalCtrl", ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
 
+  $scope.searchCategories = "";
+  
   var ref = new Firebase("https://pinterest-project.firebaseio.com/pins");
   $scope.pins = new $firebaseArray(ref);
 
